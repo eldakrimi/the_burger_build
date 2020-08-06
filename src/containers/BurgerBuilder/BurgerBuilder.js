6c6fb1cb8b5ff26217e9ac087dcab4897dddcce5
Ellen Dahlgren <ellen.dahlgren@acino.se>
2020-08-06 09:22:19 +0200

    added ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 
-import Aux from "../../hoc/Aux";
-
 class BurgerBuilder extends Component {
   render() {
     return (
-      <Aux>
+      <div>
         <div>Burger</div>
         <div>Build Controls</div>
-      </Aux>
+      </div>
     );
   }
 }
